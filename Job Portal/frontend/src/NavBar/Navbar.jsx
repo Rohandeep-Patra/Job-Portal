af062d9ab6a9ca3fdb2dcd05a35dd3295788904e
Rohandeep Patra <rohandp100@gmail.com>
2024-05-07 02:29:18 +0530

    Add Login Pop Up

--- Job Portal/frontend/src/NavBar/Navbar.jsx
+++ Job Portal/frontend/src/NavBar/Navbar.jsx
@@ -207,12 +207,12 @@ const Navbar = () => {
                           <div className="h-[1px] basis-5.5/12 w-full bg-slate-500"></div>
                         </div>
                         <div className="flex w-full justify-between items-center mt-6">
-                          <button className="border border-blue-600 px-2 py-3 rounded flex items-center justify-between">
+                          <button className="border border-[#ea4335] px-2 py-3 rounded flex items-center justify-between">
                             
                             <svg className="h-6 w-6 mx-2 fill-[#EA4335] flex items-center justify-center "  role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>
                             Login with Gmail
                           </button>
-                          <button className="border border-blue-600 px-2 py-3 rounded flex items-center justify-between">
+                          <button className="border border-[#0866ff] px-2 py-3 rounded flex items-center justify-between">
                           <svg className="h-6 w-6 mx-2 fill-[#0866FF] flex items-center justify-center "  role="img" xmlns="http://www.w3.org/2000/svg"><path d="M9.101 23.691v-7.98H6.627v-3.667h2.474v-1.58c0-4.085 1.848-5.978 5.858-5.978.401 0 .955.042 1.468.103a8.68 8.68 0 0 1 1.141.195v3.325a8.623 8.623 0 0 0-.653-.036 26.805 26.805 0 0 0-.733-.009c-.707 0-1.259.096-1.675.309a1.686 1.686 0 0 0-.679.622c-.258.42-.374.995-.374 1.752v1.297h3.919l-.386 2.103-.287 1.564h-3.246v8.245C19.396 23.238 24 18.179 24 12.044c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.628 3.874 10.35 9.101 11.647Z"/></svg>
                             Login with FaceBook
                           </button>
